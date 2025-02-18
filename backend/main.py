062b1cd24b37c35122f6170a444d30c69b82dd4a
João Teles <jant@cin.ufpe.br>
2025-02-17 22:20:23 -0300

    feat(Add): listagemSalas conflicts solved

--- backend/main.py
+++ backend/main.py
@@ -13,6 +13,8 @@ registrarBlueprints(app)
 with app.app_context():
     db.drop_all()
     db.create_all()
+
+print(app.url_map)  # Isso vai mostrar todas as rotas registradas
     
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
