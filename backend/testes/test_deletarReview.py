5dec5745ffdbe0c7162d764189377a750020c7f6
Carlos Henrique <102180736+chgs3@users.noreply.github.com>
2025-02-18 17:30:12 -0300

    Alterações

--- backend/testes/test_deletarReview.py
+++ backend/testes/test_deletarReview.py
@@ -10,15 +10,6 @@ from backend.modelo.extensao import db
 from backend.modelo.reviewSala import ReviewSala
 
 @pytest.fixture
-def app():
-    app = create_app()
-    with app.app_context():
-        db.create_all()  
-    yield app
-    with app.app_context():
-        db.drop_all() 
-
-@pytest.fixture
 def client(app):
     return app.test_client()
 
